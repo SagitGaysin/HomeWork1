b11582767b650366870b3fa4a250b8c8e955b7e5
sagit <sagitgaysin87@gmail.com>
2023-07-21 07:35:08 +0300

    Проверьте ДЗ (внес доработки, согласно замечаниям)

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
         System.out.println();
+
         System.out.println("Задача_2");
         dog = dog + 4;
         cat = cat + 4;
@@ -16,6 +17,7 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
         System.out.println();
+
         System.out.println("Задача_3");
         dog = dog - 3.5;
         cat = cat - 1.6;
@@ -24,6 +26,7 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
         System.out.println();
+
         System.out.println("Задача_4");
         var friend = 19;
         System.out.println(friend);
@@ -32,6 +35,7 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
         System.out.println();
+
         System.out.println("Задача_5");
         var frog = 3.5;
         System.out.println(frog);
@@ -42,6 +46,7 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
         System.out.println();
+
         System.out.println("Задача_6");
         var weightBoxer1 = 78.2;
         var weightBoxer2 = 82.7;
@@ -49,12 +54,14 @@ public class Main {
         System.out.println("Общий вес двух боксеров = " + totalWeight + " кг");
         System.out.println("Общий вес двух боксеров = " + totalWeight + " кг");
         System.out.println();
+
         System.out.println("Задача_7");
         var method1 = weightBoxer2 - weightBoxer1;
         System.out.println("Разница весов спортсменов через вычитание от большего веса меньшее = " + method1 + " кг");
         var method2 = weightBoxer2 % weightBoxer1;
         System.out.println("Разница весов спортсменов через функцию остатка от деления = " + method2 + " кг");
         System.out.println();
+
         System.out.println("Задача_8");
         var totalTime = 640;
         var employees = totalTime / 8;
